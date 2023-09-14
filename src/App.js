7e6e6b3858a8e752500dbfb3b60e1a5cc2aafce0
Arun Kumar R <aruntutter19@gmail.com>
2023-09-14 18:37:33 +0530

    product file added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
+import Products from "./components/Products";
+import Cart from "./components/Cart";
 
 const App = () => {
+  const [show, setShow] = useState(true);
+  const [cart, setCart] = useState([]);
+
   return (
     <>
       <Navbar />
+      <Products />
+      <Cart />
     </>
   );
 };
