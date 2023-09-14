28602dc9245488a8f330fb3d3e6b43220bc82fd7
Arun Kumar R <aruntutter19@gmail.com>
2023-09-14 17:55:01 +0530

    project data added

---
+++ src/data.js
@@ -0,0 +1,32 @@
+const list = [
+  {
+    id: 1,
+    title: "Samsung Galaxy S8",
+    price: 399.99,
+    img: "https://www.course-api.com/images/cart/phone-1.png",
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: "google pixel",
+    price: 499.99,
+    img: "https://www.course-api.com/images/cart/phone-2.png",
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: "Xiaomi Redmi Note 2",
+    price: 699.99,
+    img: "https://www.course-api.com/images/cart/phone-3.png",
+    amount: 1,
+  },
+  {
+    id: 4,
+    title: "Samsung Galaxy S7",
+    price: 599.99,
+    img: "https://www.course-api.com/images/cart/phone-4.png",
+    amount: 1,
+  },
+];
+
+export default list;
