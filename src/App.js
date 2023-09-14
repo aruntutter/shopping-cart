49e513f301524e03525326ffe0506f8f94cd7fe9
Arun Kumar R <aruntutter19@gmail.com>
2023-09-14 18:11:00 +0530

    navbar file added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 };
 
 export default App;
