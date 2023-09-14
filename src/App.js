f0be8bb3f4dc898bbe328fc3a79bca644f69c81d
Arun Kumar R <aruntutter19@gmail.com>
2023-09-15 00:17:51 +0530

    on click event created

--- src/App.js
+++ src/App.js
@@ -2,16 +2,36 @@ import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Products from "./components/Products";
 import Cart from "./components/Cart";
+import "./styles/Warning.css";
 
 const App = () => {
   const [show, setShow] = useState(true);
   const [cart, setCart] = useState([]);
+  const [warning, setWarning] = useState(false);
+
+  const handleClick = (item) => {
+    let isPresent = false;
+    cart.forEach((product) => {
+      if (item.id === product.id) isPresent = true;
+    });
+    if (isPresent) {
+      setWarning(true);
+      setTimeout(() => {
+        setWarning(false);
+      }, 2000);
+      return;
+    }
+    setCart([...cart, item]);
+  };
 
   return (
     <>
       <Navbar size={cart.length} />
-      <Products />
+      <Products handleClick={handleClick} />
       <Cart />
+      {warning && (
+        <div className="warning">Item is already added to your cart!</div>
+      )}
     </>
   );
 };
